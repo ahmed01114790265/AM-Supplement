20b2228c59a403ebc3643658465c8f26242a067f
ahmed <01114790265ahmedmohamed@gmail.com>
2025-04-30 12:17:47 +0300

    a

---
+++ AMSupplement.Domain/AMSublementDbContext.cs
@@ -0,0 +1,30 @@
+﻿using AMSupplement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace AMSupplement.Domain
+{
+    public class AMSublementDbContext : DbContext
+    {
+        IConfiguration Configuration;
+        public AMSublementDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            Configuration = configuration;  
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //    optionsBuilder.UseSqlServer(Configuration.GetConnectionString("cs"));
+        //}
+
+  
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+         
+            base.OnModelCreating(modelBuilder);
+            #region product
+          
+            # endregion
+        }
+    }
+}
