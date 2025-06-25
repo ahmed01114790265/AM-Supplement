4997a53c44d4ef895c3a42092f2206235d2c5e82
ahmed <01114790265ahmedmohamed@gmail.com>
2025-06-25 14:41:59 +0300

    1-added and migration . 2-update data base. 3-created Acount controller . 4-in Account  controller created usermanager ,signinmananger and  Rolemanager. then created constructor injection . 5-created 2viewmodel for Login ,Register,ChangPasswordViewModel. 6-in Account controller we created Login,Register , Logout  and ChangePassword Action.

--- AMSupplement.Domain/Migrations/20250625080817_Identity-1.Designer.cs
+++ AMSupplement.Domain/Migrations/20250625080817_Identity-1.Designer.cs
@@ -4,6 +4,7 @@ using AMSupplement.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AMSupplement.Domain.Migrations
 {
     [DbContext(typeof(AMSublementDbContext))]
-    partial class AMSublementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250625080817_Identity-1")]
+    partial class Identity1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
