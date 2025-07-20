2414bca8ae6a32ad0a867405c9f107d740e199e8
MahmoudAlameen <alameen2591999@gmail.com>
2025-07-20 20:32:01 +0300

    add dashboard project

---
+++ AM-Supplement.Dashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AM_Supplement.Dashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
