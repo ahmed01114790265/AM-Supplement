45ddee31f763f82e093a8ed9705a08fc9e27d35a
MahmoudAlameen <alameen2591999@gmail.com>
2025-07-23 03:51:41 +0300

    create product functionality in Dashboard product list in dashboard

---
+++ AM-Supplement.Dashboard/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using AM_Supplement.Application.Services;
+using AM_Supplement.Contracts.DTO;
+using AM_Supplement.Contracts.Services;
+using AM_Supplement.Shared.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace AM_Supplement.Dashboard.Controllers
+{
+    public class ProductController : Controller
+    {
+        IProductService ProductService;
+        IStringLocalizer<SharedResource> Localizer;
+
+        public ProductController(IProductService productService, IStringLocalizer<SharedResource> localizer)
+        {
+            ProductService = productService;
+            Localizer = localizer;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await ProductService.GetProductsList();
+            if (result.IsValid == false || result.ModelList == null)
+            {
+                ViewBag.ErrorMessage = result.ErrorMessage;
+                return View("ProductsList", new List<ProductDTO>());
+            }
+
+            return View("ProductsList", result.ModelList);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(ProductDTO productDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("AddProduct", productDTO);
+
+            }
+            var result = await ProductService.AddProduct(productDTO);
+            if (!result.IsValid)
+            {
+                ModelState.AddModelError("error", result.ErrorMessage);
+                return View("AddProduct", productDTO);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult GetAddProductView()
+        {
+            return View("AddProductForm");
+        }
+    }
+}
--- AM-Supplement.Dashboard/Program.cs
+++ AM-Supplement.Dashboard/Program.cs
@@ -1,3 +1,10 @@
+using AM_Supplement.Application;
+using AM_Supplement.Shared.Localization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+
 namespace AM_Supplement.Dashboard
 {
     public class Program
@@ -6,9 +13,58 @@ namespace AM_Supplement.Dashboard
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            //register services
+            builder.Services.AddApplication(builder.Configuration);
 
+            builder.Services.ConfigureApplicationCookie(x =>
+            {
+                x.LogoutPath = "/account/logout";
+                x.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+            });
+
+            builder.Services.AddAuthorization(x =>
+            {
+                x.AddPolicy("Admin", p => p.RequireAuthenticatedUser().RequireRole("Admin"));
+                // x.FallbackPolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            });
+
+            
+
+            //register localization services 
+            // use the below line for registering localization services if the 
+            //builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            //use this in case localized resources files in different project
+            builder.Services.AddLocalization();
+            builder.Services.AddSingleton(static serviceProvider =>
+            {
+                var factory = serviceProvider.GetRequiredService<IStringLocalizerFactory>();
+                var type = typeof(AM_Supplement.Shared.Localization.SharedResource);
+                return factory.Create(type);
+            });
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews()
+               .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+               .AddDataAnnotationsLocalization(options =>
+               {
+                   // Ensure Data Annotations use SharedResource from external layer
+                   var type = typeof(SharedResource);
+                   options.DataAnnotationLocalizerProvider = (modelType, factory) =>
+                       factory.Create(type);
+               });
+
+            var supportedCultures = new[] { "en", "ar", "fr" };
+
+            //Configure the application to detect and apply the culture for each request.
+            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            {
+                options.SetDefaultCulture("en")
+                       .AddSupportedCultures(supportedCultures)
+                       .AddSupportedUICultures(supportedCultures);
+
+                options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -23,13 +79,25 @@ namespace AM_Supplement.Dashboard
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseCors();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Product}/{action=Index}/{id?}");
 
+            app.Run();
+
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            // appy localization setting to every http request
+            var locOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
+
+            app.MapControllers();
             app.Run();
         }
     }
